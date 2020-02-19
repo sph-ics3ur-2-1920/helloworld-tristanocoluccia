644bd60df635d830cbab02a9f50ba3da4c6d4d0b
MrZebarth <kris.zebarth@ocsb.ca>
2020-02-19 08:59:09 -0500

    Initial commit

---
+++ src/start/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Name: Mr. Z
+ * Date: Feb 18, 2020
+ * Project: HelloWorld
+ * Package: start
+ * File: Main.java
+ * Description: This project will print Hello World! to the screen
+ */
+package start;
+
+/**
+ * @author k_zeb
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
